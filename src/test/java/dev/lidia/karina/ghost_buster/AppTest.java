be909dc3731e7415c48a2f1aedf8c8a59d86cc7e
karinareyes25 <karinalrg16@gmail.com>
2025-02-05 12:25:21 +0100

    Initial commit

---
+++ src/test/java/dev/lidia/karina/ghost_buster/AppTest.java
@@ -0,0 +1,18 @@
+package dev.lidia.karina.ghost_buster;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
