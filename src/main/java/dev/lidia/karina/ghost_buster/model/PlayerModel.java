39711692acbba97dc8deed3e1ab59a1a887faba0
karinareyes25 <karinalrg16@gmail.com>
2025-02-06 21:23:38 +0100

    firt test of GetPlayerName passed

--- src/main/java/dev/lidia/karina/ghost_buster/model/PlayerModel.java
+++ src/main/java/dev/lidia/karina/ghost_buster/model/PlayerModel.java
@@ -1,5 +1,25 @@
 package dev.lidia.karina.ghost_buster.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayerModel {
     
-}
+    private String playerName; // Jugador
+    private List<GhostModel> capturedGhost; // Lista de los fantasmas atrapados
+
+    // Inicializamos el jugador con su nombre, con un constructor
+
+    public PlayerModel(String playerName){
+        this.playerName = playerName;
+        this.capturedGhost = new ArrayList<>();
+        } // inicializamos la lista de fantasmas vacía
+
+        // Se obtiene el nombre de un jugando con un getter
+
+        public String getPlayerName() {
+            return playerName;
+        }
+    }
+
+
--- src/test/java/dev/lidia/karina/ghost_buster/model/PlayerModelTest.java
+++ src/test/java/dev/lidia/karina/ghost_buster/model/PlayerModelTest.java
@@ -1,18 +1,24 @@
 package dev.lidia.karina.ghost_buster.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class PlayerModelTest {
 
     private PlayerModel player;
     private GhostModel ghost;
 
-
     @BeforeEach
-    void setUp(){
+    void setUp() {
         player = new PlayerModel("Jeremih");
-        ghost = new GhostModel(1, "Casper", "Friendly", "High", "05-02-2025", "Change apparence", "Low");   
-     }
+        ghost = new GhostModel(1, "Casper", "Friendly", "High", "05-02-2025", "Change apparence", "Low");
+    }
 
+    @Test 
+    void testGetPlayerName(){
+        assertEquals("Jeremih", player.getPlayerName());
+     }
 
 }
