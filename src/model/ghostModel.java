e0c96428d1867b9833eb4c871fbf7a27ee29f5d0
karinareyes25 <karinalrg16@gmail.com>
2025-02-06 12:07:44 +0100

    code of ghost class

--- src/model/ghostModel.java
+++ src/model/ghostModel.java
@@ -1,3 +1,22 @@
 public class ghostModel {
     
+    private int id;
+    private String name;
+    private String class;
+    private int level;
+    private String date;
+    private String hability;
+    private String afinity;
+
+public ghostModel(int id, String name, String class, int level, String date, String hability, String afinity){
+
+    this.id = id;
+    this.name = name;
+    this.class = class;
+    this.level = level;
+    this.date = date;
+    this.hability = hability;
+    this.afinity = afinity;
+}
+
 }
