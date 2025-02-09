8defdbd0fe590f245226391972b98f5f6a9d4db9
Lgtf5 <lgtablasf5@gmail.com>
2025-02-10 00:24:41 +0100

    testAddGhost fault

--- src/test/java/dev/lidia/karina/ghost_buster/model/ContainerTest.java
+++ src/test/java/dev/lidia/karina/ghost_buster/model/ContainerTest.java
@@ -9,16 +9,16 @@ import java.util.List;
 
 public class ContainerTest {
     private Container container;
-    private SimulatedGhost ghost1;
-    private SimulatedGhost ghost2;
-    private List<SimulatedGhost> ghosts;
+    private Ghost ghost1;
+    private Ghost ghost2;
+    private List<Ghost> ghosts;
 
     @BeforeEach
     public void setUp() {
         ghosts = new ArrayList<>();
         container = new Container();
-        ghost1 = new SimulatedGhost(1, "Fantasma1", "TipoA", "Nivel1", "2025-01-01", "Habilidad1", "Afinidad1");
-        ghost2 = new SimulatedGhost(2, "Fantasma2", "TipoB", "Nivel2", "2025-01-02", "Habilidad2", "Afinidad2");
+        ghost1 = new Ghost(1, "Fantasma1", "TipoA", "Nivel1", "2025-01-01", "Habilidad1", "Afinidad1");
+        ghost2 = new Ghost(2, "Fantasma2", "TipoB", "Nivel2", "2025-01-02", "Habilidad2", "Afinidad2");
     }
 
     @Test
