cc4558f980c81dfad7496efdc319215c62aa561b
karinareyes25 <karinalrg16@gmail.com>
2025-02-09 18:30:43 +0100

    dev and branch feature/container update

--- src/main/java/dev/lidia/karina/ghost_buster/model/Container.java
+++ src/main/java/dev/lidia/karina/ghost_buster/model/Container.java
@@ -1,13 +1,6 @@
 package dev.lidia.karina.ghost_buster.model;
 
 import java.util.ArrayList;
-
-import java.util.List;
-
-
-
-
-import java.util.ArrayList;
 import java.util.List;
 import java.time.LocalDate;
 import java.util.stream.Collectors;
