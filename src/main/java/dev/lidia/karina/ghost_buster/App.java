4e7493bfdafa4917549f64df1033f7090ad87ee3
karinareyes25 <karinalrg16@gmail.com>
2025-02-10 02:08:52 +0100

    fix problems with the coverage, changes App.java

--- src/main/java/dev/lidia/karina/ghost_buster/App.java
+++ src/main/java/dev/lidia/karina/ghost_buster/App.java
@@ -9,12 +9,7 @@ public final class App {
 
     public class Main {
         public void main(String[] args) {
-             Container container = new Container();            // Añadir fantasmas
-            container.addGhost(
-                    new Ghost(2, "Fantasma1", "TipoA", "Nivel1", "2025-01-01", "Habilidad1", "Afinidad1"));
-            container.addGhost(
-                    new Ghost(3, "Fantasma2", "TipoB", "Nivel2", "2025-01-02", "Habilidad2", "Afinidad2"));            // Mostrar la lista de fantasmas
-            container.ghostList();
+
         }
     }
 }
